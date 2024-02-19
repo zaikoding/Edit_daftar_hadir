283a17b22895b6ce9f0931111f9074d8553c9b20
zaikoding <103026625+zaikoding@users.noreply.github.com>
2024-02-19 14:17:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Mendapatkan nilai nama formulir dari penyimpanan lokal saat halaman dimuat
-  var formName = localStorage.getItem('formName');
-
-  // Menetapkan nilai formulir ke input jika ada
-  if (formName) {
-    document.getElementById('formName').value = formName;
-  }
-});
-
+// Mendefinisikan fungsi untuk menyimpan nama formulir
 function saveFormName() {
-  // Mendapatkan nilai nama formulir dari input
-  var formName = document.getElementById('formName').value;
-
-  // Menyimpan nilai nama formulir ke penyimpanan lokal
-  localStorage.setItem('formName', formName);
-
-  // Menampilkan nilai nama formulir di elemen dengan ID 'formTitle'
-  var formTitleElement = document.getElementById('formTitle');
-  if (formTitleElement) {
-    formTitleElement.innerText = 'Nama formulir berhasil diubah menjadi: ' + formName;
-  } else {
-    console.error('Elemen dengan ID "formTitle" tidak ditemukan.');
-  }
+  // Mendapatkan nilai dari input nama formulir
+  var formName = document.getElementById("formName").value;
+  
+  // Memperbarui elemen untuk menampilkan nama formulir
+  var formTitle = document.getElementById("formTitle");
+  formTitle.innerHTML = "<h2>Nama Formulir: " + formName + "</h2>";
 }
